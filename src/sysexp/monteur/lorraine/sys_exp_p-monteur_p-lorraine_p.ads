ce03ec3257a794a08ad85f0ca43b5b3ad476dc30
Ythildir <pivardjulien@gmail.com>
2023-11-05 00:48:42 +0100

    mise en place hiérarchie lorraine

---
+++ src/sysexp/monteur/lorraine/sys_exp_p-monteur_p-lorraine_p.ads
@@ -0,0 +1,14 @@
+--  @summary
+--  Pour le format des fichiers on utilise la grammaire Lorraine.
+--  @description
+--  Pour le format des fichiers on utilise la grammaire Lorraine.
+--  @group Lorraine
+package Sys_Exp_P.Monteur_P.Lorraine_P
+   with
+      Pure           => False,
+      Preelaborate   => False,
+      Elaborate_Body => False,
+      Spark_Mode     => Off
+is
+
+end Sys_Exp_P.Monteur_P.Lorraine_P;
