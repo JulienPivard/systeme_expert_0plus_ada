2fb58202aeb678600ae066c71593530111010d2e
Ythildir <pivardjulien@gmail.com>
2023-12-02 10:29:26 +0100

    add option reporter tests unitaires pour choisir xml ou txt

--- tests_unitaires/src_tests_unitaire/executeur_de_tests.adb
+++ tests_unitaires/src_tests_unitaire/executeur_de_tests.adb
@@ -1,8 +1,10 @@
+with Ada.Command_Line;
 with Ada.Exceptions;
 with Ada.Text_IO;
 
 with AUnit.Options;
 with AUnit.Reporter.Text;
+with AUnit.Reporter.XML;
 with AUnit.Run;
 
 with Suite_De_Tests_Base;
@@ -18,6 +20,54 @@ with Suite_De_Tests_Moteur_Inference;
 with Suite_De_Tests_Jeton;
 
 procedure Executeur_De_Tests is
+   type Mode_T is (Txt_E, XML_E);
+
+   ---------------------------------------------------------------------------
+   function Creer_Reporter
+      return AUnit.Reporter.Reporter'Class;
+
+   function Creer_Reporter
+      return AUnit.Reporter.Reporter'Class
+   is
+      NB_Args : constant Integer := Ada.Command_Line.Argument_Count;
+
+      Mode : constant Mode_T :=
+         (
+            if NB_Args = 0 then
+               Txt_E
+            elsif Ada.Command_Line.Argument (1) = "xml" then
+               XML_E
+            elsif Ada.Command_Line.Argument (1) = "txt" then
+               Txt_E
+            else
+               Txt_E
+         );
+
+      Reporter_Txt : AUnit.Reporter.Text.Text_Reporter;
+      Reporter_XML : AUnit.Reporter.XML.XML_Reporter;
+   begin
+      case Mode is
+         when Txt_E =>
+            AUnit.Reporter.Text.Set_Use_ANSI_Colors
+               (
+                  Engine => Reporter_Txt,
+                  Value  => True
+               );
+         when XML_E =>
+            null;
+      end case;
+
+      return
+         (
+            case Mode is
+               when Txt_E =>
+                  Reporter_Txt,
+               when XML_E =>
+                  Reporter_XML
+         );
+   end Creer_Reporter;
+   ---------------------------------------------------------------------------
+
    --  Instanciation des procédures de tests.
    procedure Runner_Suite_Base      is new AUnit.Run.Test_Runner
       (Suite => Suite_De_Tests_Base);
@@ -51,14 +101,9 @@ procedure Executeur_De_Tests is
             Report_Successes => True,
             Filter           => null
          );
-   Reporter : AUnit.Reporter.Text.Text_Reporter;
-begin
-   AUnit.Reporter.Text.Set_Use_ANSI_Colors
-      (
-         Engine => Reporter,
-         Value  => True
-      );
 
+   Reporter : AUnit.Reporter.Reporter'Class := Creer_Reporter;
+begin
    --  Lancement des suites de tests.
    Runner_Suite_Base
       (
