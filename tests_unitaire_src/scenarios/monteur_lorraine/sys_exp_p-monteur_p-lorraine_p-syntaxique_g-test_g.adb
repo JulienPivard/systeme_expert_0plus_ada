2ef81137ff4a9323aefb4755b21cb197bb297558
Ythildir <pivardjulien@gmail.com>
2025-03-03 18:20:25 +0100

    Ajout premier test unitaire pour parseur syntaxique

--- tests_unitaire_src/scenarios/monteur_lorraine/sys_exp_p-monteur_p-lorraine_p-syntaxique_g-test_g.adb
+++ tests_unitaire_src/scenarios/monteur_lorraine/sys_exp_p-monteur_p-lorraine_p-syntaxique_g-test_g.adb
@@ -1,8 +1,12 @@
 with AUnit.Assertions;
 
+with Sys_Exp_P.Visiteur_Forme_P.Declencheur_P.Fabrique_P;
+
 package body Sys_Exp_P.Monteur_P.Lorraine_P.Syntaxique_G.Test_G is
 
    pragma Spark_Mode (Off);
+
+   package Fab_R renames Visiteur_Forme_P.Declencheur_P.Fabrique_P;
 
    ---------------------------------------------------------------------------
    overriding
@@ -35,10 +39,22 @@ package body Sys_Exp_P.Monteur_P.Lorraine_P.Syntaxique_G.Test_G is
       (T : in out Test_Fixt_T)
    is
       pragma Unreferenced (T);
+
+      F : constant Fab_R.Fabrique_T := Fab_R.Creer;
+
+      Syntaxique : Syntaxique_T := Creer
+         (
+            Nom_Fichier => "inutile",
+            Fabrique    => F
+         );
+
+      Regles : constant Base_De_Regles_T := Syntaxique.Parser;
+      pragma Unreferenced (Syntaxique);
+      --  Le parseur syntaxique n'est plus utile.
    begin
       AUnit.Assertions.Assert
          (
-            Condition => False,
+            Condition => Regles.Possede_Successeur,
             Message   => "Creation du parseur lexical"
          );
    end Test_Creer;
