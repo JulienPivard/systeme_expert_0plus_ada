53ec750789ed5795581620ac4bf0ff53d1f7f75b
Ythildir <pivardjulien@gmail.com>
2025-03-16 20:33:51 +0100

    suppression with inutile

--- tests_unitaire_src/scenarios/monteur_lorraine/sys_exp_p-monteur_p-lorraine_p-faux_syntaxique_p-test_p.ads
+++ tests_unitaire_src/scenarios/monteur_lorraine/sys_exp_p-monteur_p-lorraine_p-faux_syntaxique_p-test_p.ads
@@ -1,5 +1,4 @@
 with Sys_Exp_P.Monteur_P.Lorraine_P.Syntaxique_G.Test_G;
-with Sys_Exp_P.Monteur_P.Lorraine_P.Faux_Syntaxique_P;
 
 package Sys_Exp_P.Monteur_P.Lorraine_P.Faux_Syntaxique_P.Test_P is new
    Sys_Exp_P.Monteur_P.Lorraine_P.Faux_Syntaxique_P.Test_G;
