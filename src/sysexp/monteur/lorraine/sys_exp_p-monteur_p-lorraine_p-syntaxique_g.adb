810f5dcb978d39339e077af3e3672720dca14c45
Ythildir <pivardjulien@gmail.com>
2025-03-30 22:33:53 +0200

    fix indentation

--- src/sysexp/monteur/lorraine/sys_exp_p-monteur_p-lorraine_p-syntaxique_g.adb
+++ src/sysexp/monteur/lorraine/sys_exp_p-monteur_p-lorraine_p-syntaxique_g.adb
@@ -324,16 +324,16 @@ package body Sys_Exp_P.Monteur_P.Lorraine_P.Syntaxique_G is
             Bloc_Faire_Message_Erreur :
             declare
                Message : constant Rapport_D_Erreur_T := Creer
-                     (
-                        Message => This.Faire_Message_Erreur
-                           (Message => "attendu : '" & Fin_Expr & "'")
-                     );
+                  (
+                     Message => This.Faire_Message_Erreur
+                        (Message => "attendu : '" & Fin_Expr & "'")
+                  );
             begin
-            Resultat       := Resultat_Parseur_T'
-               (
-                  Reussie          => False,
-                  Rapport_D_Erreur => Message
-               );
+               Resultat := Resultat_Parseur_T'
+                  (
+                     Reussie          => False,
+                     Rapport_D_Erreur => Message
+                  );
             end Bloc_Faire_Message_Erreur;
          end if;
 
